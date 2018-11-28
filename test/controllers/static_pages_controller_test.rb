f488d17a2f5cf261a639210167507eaf6ad42c0e
Ungolko <Ungolko@gmail.com>
2018-11-28 12:25:20 +0300

    Add a Contact page (Exercise 3.2)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,7 +6,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
-  
+
   test "should get home" do
     get root_url
     assert_response :success
@@ -25,4 +25,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
   end
 
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
 end
